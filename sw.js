f4b7dbddc1f631bc1f77a16ed7bea25d5fdef46b
adithya-astro <adithyanpallikadavath@gmail.com>
2026-02-10 10:17:31 +0530

    update-2

--- sw.js
+++ sw.js
@@ -1,10 +1,25 @@
-const CACHE_NAME = 'Task Manager';
+const CACHE_NAME = 'task-manager-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon.png'
+];
 
+// Install event: Caches the basic files
 self.addEventListener('install', (event) => {
-  console.log('Service Worker installed');
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
 });
 
+// Fetch event: Required for the Install prompt to show
 self.addEventListener('fetch', (event) => {
-  // Logic for offline support can be added here
-  event.respondWith(fetch(event.request));
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
 });
